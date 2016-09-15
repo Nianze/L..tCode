f1afc23be897a871f6f625b32f443509dcfe050b
Joseph <daoxinzhishui@gmail.com>
2016-09-15 19:38:57 -0400

    added

--- Java/maxArea.java
+++ Java/maxArea.java
@@ -4,6 +4,15 @@ public class Solution {
      * @return: an integer
      */
     public int maxArea(int[] heights) {
- 	       
+ 	int left = 0, right = heights.length - 1, ans = 0;
+        while(left < right) {
+            ans = Math.max(ans, (right - left) * Math.min(heights[left], heights[right]));
+            if (heights[left] < heights[right]){
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
     }
 }
