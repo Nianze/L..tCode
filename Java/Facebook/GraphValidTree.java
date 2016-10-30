491025cbba737ae309f3497d7fec940d7f5d7ca9
Joseph <daoxinzhishui@gmail.com>
2016-10-30 04:38:09 -0400

    update

--- Java/Facebook/GraphValidTree.java
+++ Java/Facebook/GraphValidTree.java
@@ -4,9 +4,102 @@ public class GraphValidTree {
     // 1. whether these is loop
     // 2. whether connected componets is more than 1
     
-    // DFS    
+    // DFS
+    // time: O(max(E,n)) space: O(n) for adjacency list
+    public boolean validTree(int n, int[][] edges) {
+        int[] visited = new int[n];
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<Integer>());
+        }
+        for (int[] e : edges) {
+            adjList.get(e[0]).add(e[1]);
+            adjList.get(e[1]).add(e[0]);
+        }
+        if (hasCycle(-1, 0, visited, edges)) { return false; } // cycle case
+        for (int i : visited) { if (i == 0) { return false; } } // not single connected components
+        return true;
+    }
     
-    // BFS using queue
+    private boolean hasCycle(int prev, int cur, int[] visited, List<List<Integer>> adjList) {
+        visited[cur] = 1; // 1 means current vertex is being visited
+        for (Integer succ : adjList.get(cur)) {
+            if (succ == prev) continue; // exclude curr's prev node
+            if (visited[succ] == 1) { return true; } // has cycle
+            if (visited[succ] == 0) {
+                if (hasCycle(cur, succ, visited, adjList)) { return true; }
+            } 
+        }
+        visited[cur] = 2; // complete visiting
+        return false;
+    }
+    
+    // BFS using deque
+    // O(max(E,n)), space: O(n) for adjacency list
+    public validTree(int n, int[][] edges) {
+        int[] visited = new int[n];
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<Integer>());
+        }
+        for (int[] e : edges) {
+            adjList.get(e[0]).add(e[1]);
+            adjList.get(e[1]).add(e[0]);
+        }
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.addLast(0);
+        visited[0] = 1; // mark curr node as being visited
+        while (!queue.isEmpty()) {
+            int cur = queue.removeFirst();
+            for (Integer succ : adjList.get(cur)) {
+                if (visited[succ] == 1) { return false; } // has cycle
+                if (visited[succ] == 0) {
+                    visited[succ] = 1;
+                    queue.addLast(succ);
+                }
+            }
+            visited[cur] = 2; // compete visiting
+        }
+    }
+    
+    // union-find with compression and merge by rank
+    // time: O(E), space: O(n) for union-find set
+    class UnionFind {
+        int[] parent;
+        int[] rank;
+        int count;
 
-    // union-find
+        UionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) { parent[i] = i; } // initially each node's paren is itself
+        }
+        int find(int x) {
+            if (x != parent[x]) {
+                x = find(parent[x]); // path compression
+            }
+            return parent[x];
+        }
+        boolean union(int x, int y) {
+            int X = find(x), Y = find(y);
+            if (X == Y) { return false; }
+            if (rank[Y] < rank[X]) { parent[Y] = X; } // Y is lower
+            else if (rank[X] < rank[Y]) { parent[X] = Y;} // X is lower
+            else {  // rank of X, Y is the same
+                parent[Y] = X;
+                rank[X]++;
+            }
+            count--;
+            return true;
+        }
+    }    
+    public boolean validTree(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        for (int[] edge : edges) {
+            int x = edge[0], y = edge[1];
+            if (!uf.union(x, y)) { return false; } // has cycle
+        }
+        return uf.count == 1;
+    }
 }
--- Java/Facebook/Note.md
+++ Java/Facebook/Note.md
@@ -37,3 +37,64 @@ and that's more work behind the scene
 element** during iteration.
   * Linked List supports null element
 
+## Thoughts on inner piece, and why CS/Tech ##
+
+I have to admit, I am kind of tired. Tired the getting up early. Tired
+about thinking for my future, tired about knowing who I am. And tired about
+thinking up who I used to be.
+
+But just now, suddenly, after reading an Harvard Law PhD's post on Zhihu,
+it occurred to me that, to forget is to betray.
+
+Why I choose to be here? Why I want to become a programmer?
+
+The answer, which I forget, is actually very simple.
+
+I used to think that coding is fun, considering coding as a fascinating job,
+which I could be working along for a whole day and still smile for any coding
+challenges. I used to think hackers are so cool, who are able to dig out all
+the untoldable information hidden in the internet and get some first-hand
+knowledge, predicting the trend of the world. I used to dream to be a creator,
+using my own skills and knowledge to create complex machine.
+
+And what about now? All these thoughts are gone. I forget them all, with only one
+thing left behind: I know I have to prepare for the interview, or I will become
+a free man without any job. I do not want to be a jobless man. But I can not
+control myself to fight for my dream, eighter. Recently, I can not get up
+early enough to do the coding challenge, because every night when I go to bed,
+it usually is 5:00am in the morning already. I'm afraid of being jobless,
+and have a lot of pressure deep in my heart, so somehow I choose to escape.
+
+I know this is not a good atitude toward life. But I just can't help. So I
+write down all of this into this note, somehow hoping that expressing
+myself may relief my mind and make things better.
+
+Human beings are emotional animals. For a long time, without reading any
+literary book or watching any deep-thought movie, my mind becomes barren and
+torpid. So these days I just buried myself into entertainment videos, somehow
+relieved my mind by escaping the cold reality. But time flies, unstoppable.
+The deadline will always come, no sooner, no later.
+
+I know it's not a good time to escape any more. I know it's time to fight. With
+all my effort. I should firstly let myself know, why the hell I really
+want to do the job?
+
+For money? For living? For anyone else also choose to be?
+No, they are add-ons. They are never my initial intention.
+
+I choose to be so, because I really think this is a good career that worth my
+fighting for a life long time. I do like to **dig out** how things functioning
+under the surface. I do want to **create** something big and elegant. I do like
+a **freedom** lifestyle. Those are my believes. After searching for all the jobs,
+I've found that coding is a really good fit: I can learn how every thing
+inside the internet works and influence every one's life; I can create elegant
+code that make big impact to people with just a laptop and an editor; I can
+live in a wonderful lifestyle with so much freedom and schedule my time to
+learn everything I get interested about in my spare time.
+
+All of this, is exactly how I can dream about for an ideal job and ideal lifestyle.
+All I care about is that I can live with passion and fun. Life is so short,
+I don't want to waste it on something I don't like, or something I am to regret about.
+
+So I decided to fight for my future, with everything I have.
+Everything!
